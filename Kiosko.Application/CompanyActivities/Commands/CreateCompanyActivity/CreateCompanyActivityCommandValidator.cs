f250833d20145eb4e018316455e078ae4c81eea8
Alex Campos <alexcamposve@gmail.com>
2023-06-04 14:15:41 -0300

    various

---
+++ Kiosko.Application/CompanyActivities/Commands/CreateCompanyActivity/CreateCompanyActivityCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiosko.Application.CompanyActivities.Commands.CreateCompanyActivity
+{
+    public class CreateCompanyActivityCommandValidator : AbstractValidator<CreateCompanyActivityCommand>
+    {
+        public CreateCompanyActivityCommandValidator()
+        {
+            RuleFor(v => v.ActivityName)
+                .MaximumLength(200)
+                .NotEmpty();
+        }
+    }
+}
---
+++ Kiosko.Application/CompanyActivities/EventHandlers/CompanyActivityCreatedEventHandler.cs
@@ -0,0 +1,23 @@
+﻿using Kiosko.Domain.Events;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Kiosko.Application.CompanyActivities.EventHandlers
+{
+    public class CompanyActivityCreatedEventHandler : INotificationHandler<CompanyActivityCreatedEvent>
+    {
+        private readonly ILogger<CompanyActivityCreatedEventHandler> _logger;
+
+        public CompanyActivityCreatedEventHandler(ILogger<CompanyActivityCreatedEventHandler> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Handle(CompanyActivityCreatedEvent notification, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("CleanArchitecture Domain Event: {DomainEvent}", notification.GetType().Name);
+
+            return Task.CompletedTask;
+        }
+    }
+}
